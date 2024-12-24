fb593e44c34c4ecb74d963342efa58cd3d9d34d4
Kleber Vales <ksvales@outlook.pt>
2024-12-24 09:44:25 -0300

    resolucao do exercicio

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -1,10 +1,30 @@
 package solution;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
 
-	}
+            byte[] hashBytes = md.digest(input.getBytes());
 
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                hexString.append(String.format("%02x", b));
+            }
+
+            System.out.println(hexString.toString());
+
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("MD5 algorithm not found.");
+        }
+    }
 }
+
